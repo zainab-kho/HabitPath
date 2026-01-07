dcae3d63d63d79f86025e4bd408dcae82737318b
Zainab Khoshnaw <khoshnawzainab@gmail.com>
2026-01-07 16:55:08 -0600

    fix: use correct Supabase anon key variable

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,7 +4,7 @@ import { createClient } from '@supabase/supabase-js'
 import 'react-native-url-polyfill/auto'
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY! 
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     auth: {
